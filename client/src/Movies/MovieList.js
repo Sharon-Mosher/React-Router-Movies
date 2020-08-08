cfbd9df4e5412b258e3649a863948b95b6d926f7
Sharon Mosher <sharon.a.mosher@gmail.com>
2020-08-07 23:13:08 -0400

    fixed small error

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -15,7 +15,7 @@ function MovieDetails({ movie }) {
   const { title, director, metascore } = movie;
 
   return (
-    <Link to={'/movies/${movie.id}'}>
+    <Link to={`/movies/${movie.id}`}>
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
